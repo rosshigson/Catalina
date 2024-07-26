6024eb1c45e014c30603b67db41c0147a45213c5
rosshigson <rosshigson@gmail.com>
2024-07-26 16:37:42 +1000

    Warn of missing binaries
    The "use_catalina" scripts now warn the user if there are no binary files in the Catalina installation

--- use_catalina.bat
+++ use_catalina.bat
@@ -8,5 +8,18 @@ if NOT "%LCCDIR%" == "" goto lccdir_set
 set LCCDIR=%CD%
 :lccdir_set
 set PATH=%LCCDIR%\bin;%PATH%;%LCCDIR%\GnuWin32\bin;
+if EXIST "%LCCDIR%\bin\catalina.exe" goto done
+echo.
+echo ==========================================================================
+echo           IMPORTANT: THIS INSTALLATION OF CATALINA HAS NO BINARIES
+echo.
+echo    If you downloaded Catalina from GitHub, then there should be a binary
+echo    asset associated with the release containing Windows binaries. Or you
+echo    can rebuild Catalina from source - for more details, see the document
+echo    README.md and BUILD.TXT
+echo.
+echo ==========================================================================
+echo.
+:done
 call catalina_env.bat
 cd %USERPROFILE%
