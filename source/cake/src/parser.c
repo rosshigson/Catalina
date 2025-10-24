867f708f5e265d7d953ad9c8261a688f897fef47
rosshigson <rosshigson@gmail.com>
2025-10-24 11:23:01 +1100

    Update parser.c
    Fix message formatting.

--- source/cake/src/parser.c
+++ source/cake/src/parser.c
@@ -453,7 +453,7 @@ _Bool compiler_diagnostic(enum diagnostic_id w,
         else if (is_note)
             printf("note: ");
 
-        printf("%s", buffer);
+        printf("%s"\n, buffer);
     }
     else
     {
