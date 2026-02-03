c93a0c58e6a71267bc724b3ce9b1d4c12a98763f
rosshigson <rosshigson@gmail.com>
2026-02-03 11:42:40 +1100

    Fix race condition in Multi-threaded Lua example 6
    - Multi-threaded Lua example 6 (i.e. ex6.lua) contained a race condition because it was using condition variables as proxies for the conditions themselves - this behaviour is undefined, and worked on some platforms but not others. Now this example uses an explicit boolean condition in addition to the condition variable.

--- demos/catalyst/lua-5.5.0/test/ex6.lua
+++ demos/catalyst/lua-5.5.0/test/ex6.lua
@@ -3,57 +3,89 @@
 --
 
 -- make sure we are being executed with mlua (or mluax)
-if not threads then                                                             
-  print("threads not supported - use mlua or mluax");
-  print("(see 'Lua on the Propeller 2 with Catalina')");
+if not threads then
+  print("threads not supported - use mlua or mluax")
+  print("(see 'Lua on the Propeller 2 with Catalina')")
   if propeller then
-    propeller.msleep(100);
+    propeller.msleep(100)
   end
-  os.exit();                                                                    
-end                                                                             
+  os.exit()
+end
 
-t = require "threads"
+t = threads
+
 -- we want four simultaneous threads
 t.workers(4)
+
 -- here is how we will identify them, which
 -- is also a string for each one to output
 eeny  = "eeny "
 meeny = "meeny "
 miney = "miney "
 mo    = "mo\n"
+
 -- we need four condition variables
 -- (one for each thread)
 t.condition(eeny)
 t.condition(meeny)
 t.condition(miney)
 t.condition(mo)
--- geerate a thread function
-function Thread(me, next, count)
+
+-- generate a thread function
+function Thread(me, next, count, last)
    return function()
      t = threads
      repeat
         t.lock(me)
+        t.update(me, true)
         t.wait(me)
-        t.output(me)
+        t.update(me, false)
         t.unlock(me)
-        t.msleep(250) -- minimize race condition!
-        t.signal(next)
-        count = count-1
+        t.output(me)
+        count = count - 1
+        if (count > 0) or not last then
+           next_ready = false
+           while not next_ready do
+              t.lock(next)
+              next_ready = t.shared(next)
+              t.unlock(next)
+           end
+           t.signal(next)
+        end
      until count == 0
-     return nil
    end
 end
--- create the threads
+
+-- wait till a thread is "ready"
+-- (i.e. waiting on its condition variable)
+function wait_till_ready(this)
+   local this_ready = false
+   while not this_ready do
+      t.lock(this)
+      this_ready = t.shared(this)
+      t.unlock(this)
+   end
+   return this_ready
+end
+
 count = 20
-t.new(Thread(eeny,  meeny, count))
-t.new(Thread(meeny, miney, count))
-t.new(Thread(miney, mo,    count))
-t.new(Thread(mo,    eeny,  count))
+
+-- create the threads
+t.new(Thread(eeny,  meeny, count, false))
+t.new(Thread(meeny, miney, count, false))
+t.new(Thread(miney, mo,    count, false))
+t.new(Thread(mo,    eeny,  count, true))
+
+-- wait till eeny is "ready"
+wait_till_ready(eeny)
+
 -- kick things off
 t.signal(eeny)
+
 -- wait for threads to complete
 t.wait()
 
 -- Catalina/Catalyst may clear the screen on program exit
 print("\nPress ENTER to terminate")
-any = io.stdin:read() 
+any = io.stdin:read()
+
